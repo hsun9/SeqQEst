fbf6ef105fa7d808c90bfa90d40b1dc3bc667e16
Hua Sun <33794673+hsun9@users.noreply.github.com>
2020-04-21 11:48:19 -0500

    Add files via upload

--- scriptsQC/qc-L2.bamReadcount2vaf.pl
+++ scriptsQC/qc-L2.bamReadcount2vaf.pl
@@ -1,10 +1,10 @@
 =head1
-
-	Author: Hua Sun
-	Email: hua.sun@wustl.edu/hua.sun229@gmail.com
 	
+	Author: Hua Sun
+    Email: hua.sun@wustl.edu/hua.sun229@gmail.com
+	
+	10/2/2019 add total depth in output
 	1/2/2019
-
 	
 	perl bamReadcount_vaf.pl -s sample -l loci.txt input > output
 	
@@ -20,40 +20,22 @@
 	1       898870  C       82      =:0:0.00:0.00:0.00:0:0:0.00:0.00:0.00:0:0.00:0.00:0.00  A:0:0.00:0.00:0.00:0:0:0.00:0.00:0.00:0:0.00:0.00:0.00  C:49:58.90:32.76:36.84:6:43:0.50:0.00:4.61:7:0.14:75.86:0.49    G:0:0.00:0.00:0.00:0:0:0.00:0.00:0.00:0:0.00:0.00:0.00  T:33:60.00:30.33:37.00:2:31:0.54:0.01:30.88:3:0.22:76.00:0.51   N:0:0.00:0.00:0.00:0:0:0.00:0.00:0.00:0:0.00:0.00:0.00
 	
 	//output
-	#sample chr start end ref alt	readcound_ref	readcount_alt	vaf
+	#sample chr start end ref alt	total_readcount	readcound_ref	readcount_alt	vaf
 	
 =cut
 
 use strict;
 use Getopt::Long;
 
-my $sample = '';
-my $loci = '';
-my $help;
+my $sample;
+my $loci;
 GetOptions(
 	"l:s" => \$loci,
-	"s:s" => \$sample,
-	"help" => \$help
+	"s:s" => \$sample
 );
-
-
-die `pod2text $0` if ($help || @ARGV==0 || $loci eq '' || $sample eq '');
-
-# check file
-unless (-e $loci) {
-	print "[ERROR] The $loci doesn't exist!\n";
-	die;
-}
 
 # from bamreadcount result
 my $file = shift;
-
-unless (-e $file) {
-	print "[ERROR] The $file doesn't exist!\n";
-	die;
-}
-
-
 my @bamRC=`cat $file`;
 
 # from loci
@@ -96,7 +78,8 @@ sub ExtractVAF
 	my $rc_ref = $arr[2];
 	
 	my ($r_ref, $r_alt);
-	my $nucleotide;
+	my $ref_nucleotide;
+	my $alt_nucleotide;
 	
 	$r_ref=0;
 	$r_alt=0;
@@ -105,24 +88,27 @@ sub ExtractVAF
 		$r_ref = $1 if ($val=~/\t$rc_ref\:(\d+)\:/);
 		$r_alt = $1 if ($val=~/\t\+$alt\:(\d+)\:/);
 	} elsif ($alt eq '-'){
-		$nucleotide = substr($ref, 0, 1);
-		$r_ref = $1 if ($val=~/\t$nucleotide\:(\d+)\:/);
+		$ref_nucleotide = substr($ref, 0, 1);
+		$r_ref = $1 if ($val=~/\t$ref_nucleotide\:(\d+)\:/);
 		$r_alt = $1 if ($val=~/\t\-$ref\:(\d+)\:/);	
 	} else {
-		$r_ref = $1 if ($val=~/\t$ref\:(\d+)\:/);
-		$r_alt = $1 if ($val=~/\t$alt\:(\d+)\:/);
+		$ref_nucleotide = substr($ref, 0, 1);
+		$alt_nucleotide = substr($alt, 0, 1);
+		$r_ref = $1 if ($val=~/\t$ref_nucleotide\:(\d+)\:/);
+		$r_alt = $1 if ($val=~/\t$alt_nucleotide\:(\d+)\:/);
 	}
-		
-	my $sum = $r_ref + $r_alt;
+	
+	# total read count
+	my $total_depth = $r_ref + $r_alt;
 	
 	my $vaf;
-	if ($sum==0){
+	if ($total_depth==0){
 		$vaf = 0;
 	} else {
-		$vaf = sprintf("%.2f", $r_alt/($sum)*100);
+		$vaf = sprintf("%.2f", $r_alt/($total_depth)*100);
 	}
 	
-	return "$r_ref\t$r_alt\t$vaf";
+	return "$total_depth\t$r_ref\t$r_alt\t$vaf";
 }
 
 
